28ce6c4502846d26e8afd199db0e9f6c2d76b6ab
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-06-18 19:04:31 -0400

    start deisplay one book

---
+++ client/src/components/DisplayOne.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const DisplayOne = (props) => {
+    return (
+        <div>
+            <h2>One Book</h2>
+        </div>
+    );
+};
+
+export default DisplayOne;
