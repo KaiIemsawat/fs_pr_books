5bf915dc77814329e2fc440a1f8ed62955f169ed
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-06-18 17:56:50 -0400

    half way on create form

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -9,8 +9,88 @@ const AddBook = (props) => {
         digitalAvailability: false,
         description: "",
     });
+    const [errors, setErrors] = useState({});
 
-    return <div>AddBook</div>;
+    const changeHandler = (e) => {
+        //
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        //
+    };
+
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                <div>
+                    <label>Title : </label>
+                    <input
+                        type="text"
+                        name="title"
+                        onChange={changeHandler}
+                        value={book.title}
+                    />
+                    {errors.title ? (
+                        <p className="text-danger">{errors.title.message}</p>
+                    ) : null}
+                </div>
+                <div>
+                    <label>Author : </label>
+                    <input
+                        type="text"
+                        name="author"
+                        onChange={changeHandler}
+                        value={book.author}
+                    />
+                    {errors.author ? (
+                        <p className="text-danger">{errors.author.message}</p>
+                    ) : null}
+                </div>
+                <div>
+                    <label>Page : </label>
+                    <input
+                        type="number"
+                        name="page"
+                        onChange={changeHandler}
+                        value={book.page}
+                    />
+                    {errors.page ? (
+                        <p className="text-danger">{errors.page.message}</p>
+                    ) : null}
+                </div>
+                <div>
+                    <label>Description : </label>
+                    <textarea
+                        type="text"
+                        name="description"
+                        onChange={changeHandler}
+                        value={book.description}
+                    />
+                    {errors.description ? (
+                        <p className="text-danger">
+                            {errors.description.message}
+                        </p>
+                    ) : null}
+                </div>
+                <div>
+                    <label>Digital Version Availability</label>
+                    <input
+                        type="checkbox"
+                        name="digitalAvailability"
+                        onChange={changeHandler}
+                        value={book.digitalAvailability}
+                    />
+                    {errors.description ? (
+                        <p className="text-danger">
+                            {errors.digitalAvailability.message}
+                        </p>
+                    ) : null}
+                </div>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 };
 
 export default AddBook;
